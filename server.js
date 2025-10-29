849eb62cfdd3d7f43516dc890b2549e6176874a9
Amal <amalvishnukvk3@gmail.com>
2025-10-29 13:50:38 +0530

    cors updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 // app.use(cors());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://employee-management-system-i.netlify.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
 }));
 app.use('/api/employees', employeeRoutes);
