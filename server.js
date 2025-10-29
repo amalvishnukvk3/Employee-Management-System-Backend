d3fff9bc611c787335fabb771b5c492d7dc7b627
Amal <amalvishnukvk3@gmail.com>
2025-10-29 13:38:17 +0530

    cors added

--- server.js
+++ server.js
@@ -9,7 +9,11 @@ connectDB();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 app.use('/api/employees', employeeRoutes);
 app.get('/', (req, res) => {
     res.send('Employee Management System Backend Running...');
